89650dc2233c88a5928e8c00e54faba182001ff1
Ratan Varsha <137422071+Varshahsrav@users.noreply.github.com>
2025-07-10 17:36:30 +0530

    Add files via upload

---
+++ happybday.py
@@ -0,0 +1,7 @@
+import pywhatkit
+import datetime
+phone_number="Type number to whom you want to send msg"
+message="Happy birthday"
+send_hour=#Type the number you want to send msg example:12
+send_minute=#Type the number you want to send msg example:00
+pywhatkit.sendwhatmsg(phone_number,message,send_hour,send_minute)
